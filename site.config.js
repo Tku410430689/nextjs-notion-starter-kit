7163b5aeb5fe04abe6d1e434c8208535f9b2a7cb
Tku410430689 <99944796+Tku410430689@users.noreply.github.com>
2022-02-18 13:40:56 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '4af79745a9a24ac0ba7e0df486dad263',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'TKU AEROSPACE MEN',
+  domain: 'https://tku-aerospace-men.notion.site/TKU-AEROSPACE-MEN-4af79745a9a24ac0ba7e0df486dad263',
+  author: 'aerospace person',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'non',
+  github: 'Tku410430689',
+  linkedin: 'non',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
